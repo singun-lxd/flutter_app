ee1aeda0e9081eef508152dc1399ef0506511517
liuxindian <liuxindian@nemo-inc.com>
2019-08-08 20:17:14 +0800

    * localizations step2

--- lib/localizations.dart
+++ lib/localizations.dart
@@ -1,59 +1,95 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:intl/intl.dart';
 
-void main() => runApp(LocaleApp());
+import 'l10n/messages_all.dart';
 
-class LocaleApp extends StatelessWidget {
+void main() => runApp(Demo());
+
+class Demo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      onGenerateTitle: (BuildContext context) => DemoLocalizations.of(context).title,
       localizationsDelegates: [
-        // ... app-specific localization delegate[s] here
+        const DemoLocalizationsDelegate(),
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
         GlobalCupertinoLocalizations.delegate,
       ],
       supportedLocales: [
         const Locale('en'), // English
-        const Locale('he'), // Hebrew
-        const Locale.fromSubtags(languageCode: 'zh'), // generic Chinese 'zh'
+        const Locale('zh'), // generic Chinese 'zh'
         const Locale.fromSubtags(languageCode: 'zh', scriptCode: 'Hans'), // generic simplified Chinese 'zh_Hans'
         const Locale.fromSubtags(languageCode: 'zh', scriptCode: 'Hant'), // generic traditional Chinese 'zh_Hant'
         const Locale.fromSubtags(languageCode: 'zh', scriptCode: 'Hans', countryCode: 'CN'), // 'zh_Hans_CN'
         const Locale.fromSubtags(languageCode: 'zh', scriptCode: 'Hant', countryCode: 'TW'), // 'zh_Hant_TW'
         const Locale.fromSubtags(languageCode: 'zh', scriptCode: 'Hant', countryCode: 'HK'), // 'zh_Hant_HK'
       ],
-      title: 'locale test',
-      home: Localizations(),
+      // Watch out: MaterialApp creates a Localizations widget
+      // with the specified delegates. DemoLocalizations.of()
+      // will only find the app's Localizations widget if its
+      // context is a child of the app.
+      home: DemoApp(),
     );
   }
 }
 
-class Localizations extends StatefulWidget {
-  @override
-  LocalizationsState createState() => LocalizationsState();
-}
-
-
-class LocalizationsState extends State<Localizations> {
-
+class DemoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(MaterialLocalizations.of(context).licensesPageTitle),
+        title: Text(DemoLocalizations.of(context).title),
       ),
-      body: Material(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Text(MaterialLocalizations.of(context).dialogLabel),
-              Text(MaterialLocalizations.of(context).popupMenuLabel),
-            ],
-          ),
-        )
-      )
+      body: Center(
+        child: Text(DemoLocalizations.of(context).content),
+      ),
     );
   }
+}
+
+class DemoLocalizations {
+  static Future<DemoLocalizations> load(Locale locale) {
+    final String name = locale.countryCode.isEmpty ? locale.languageCode : locale.toString();
+    final String localeName = Intl.canonicalizedLocale(name);
+
+    return initializeMessages(localeName).then((_) {
+      Intl.defaultLocale = localeName;
+      return DemoLocalizations();
+    });
+  }
+
+  static DemoLocalizations of(BuildContext context) {
+    return Localizations.of<DemoLocalizations>(context, DemoLocalizations);
+  }
+
+  String get title {
+    return Intl.message(
+      'Hello World',
+      name: 'title',
+      desc: 'Title for the Demo application',
+    );
+  }
+
+  String get content {
+    return Intl.message(
+      'Content',
+      name: 'content',
+      desc: 'Content for the Demo application',
+    );
+  }
+}
+
+class DemoLocalizationsDelegate extends LocalizationsDelegate<DemoLocalizations> {
+  const DemoLocalizationsDelegate();
+
+  @override
+  bool isSupported(Locale locale) => ['en', 'zh'].contains(locale.languageCode);
+
+  @override
+  Future<DemoLocalizations> load(Locale locale) => DemoLocalizations.load(locale);
+
+  @override
+  bool shouldReload(DemoLocalizationsDelegate old) => false;
 }
